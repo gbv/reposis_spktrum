fd39e0f2bc98bcc590bde89d412bf29da9a7371b
Andre Herzog <andre.herzog@gbv.de>
2023-06-08 11:15:00 +0200

    FDRSPK-61 Added english translation of static pages

--- src/main/resources/META-INF/resources/js/project.js
+++ src/main/resources/META-INF/resources/js/project.js
@@ -19,12 +19,22 @@ $(document).ready(function() {
 
   // replace placeholder USERNAME with username
   var userID = $("#currentUser strong").html();
-  var localHref = 'http://localhost:8181/mir/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
-  $("a[href='http://localhost:8181/mir/servlets/solr/select?q=createdby:USERNAME']").attr('href', localHref);
+  var localHref = 'http://localhost:18601/mir/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='http://localhost:18601/mir/servlets/solr/select?q=createdby:USERNAME']").attr('href', localHref);
   var testHref = 'https://reposis-test.gbv.de/spktrum/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
   $("a[href='https://reposis-test.gbv.de/spktrum/servlets/solr/select?q=createdby:USERNAME']").attr('href', testHref);
   var prodHref = 'https://spktrum.spk-berlin.de/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
   $("a[href='https://spktrum.spk-berlin.de/servlets/solr/select?q=createdby:USERNAME']").attr('href', prodHref);
+
+  // replace URL if user language is english
+  if ($("html").attr("lang") === 'en') {
+    $("a[href='http://localhost:18601/spktrum/content/rights/Publikationsvertrag.pdf']").attr(
+        'href', 'http://localhost:18601/spktrum/content/rights/Publikationsvertrag_eng.pdf');
+    $("a[href='https://reposis-test.gbv.de/spktrum/content/rights/Publikationsvertrag.pdf']").attr(
+      'href', 'https://reposis-test.gbv.de/spktrum/content/rights/Publikationsvertrag_eng.pdf');
+    $("a[href='https://spktrum.spk-berlin.de/content/rights/Publikationsvertrag.pdf']").attr(
+        'href', 'https://spktrum.spk-berlin.de/content/rights/Publikationsvertrag_eng.pdf');
+  }
 
 });
 
@@ -33,3 +43,4 @@ $(document).ready(function() {
 //   $("select#genre option[value='series']").remove();
 //   $("select#genre option[value='journal']").remove();
 // });
+
